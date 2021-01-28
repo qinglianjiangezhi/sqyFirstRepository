3995c6ed759a8e9679e82b5e6fcb26bc2ede2ecd
offcn-java <aaa@bbb>
2021-01-28 21:07:08 +0800

    修改输出内容

--- Test1.java
+++ Test1.java
@@ -1,7 +1,8 @@
-public class Test1{
+public class Test1 {
 
-	public static void main(String[] args){
-		System.out.println();
-	}
+    public static void main(String[] args) {
+
+        System.out.println("今天的代码终于快完了,哈哈哈哈");
+    }
 
 }
